142e0550e6d744c5524452267a0fb69abb45dcae
Lychi <48879175+lychileng@users.noreply.github.com>
2024-06-22 17:51:01 +0800

    Update select_best_proxy.js

--- select_best_proxy.js
+++ select_best_proxy.js
@@ -1,12 +1,10 @@
-(async () => {
-  let proxies = $config.getProxies();
-  let proxyLatencies = await Promise.all(proxies.map(async proxy => {
-    let latency = await $http.ping(proxy.server, { timeout: 5000 });
-    return { proxy, latency };
-  }));
+const proxies = $config.getProxies();
+const proxyLatencies = proxies.map(proxy => {
+  const latency = $http.ping(proxy.server, { timeout: 5000 });
+  return { proxy, latency };
+});
 
-  proxyLatencies.sort((a, b) => a.latency - b.latency);
-  
-  let bestProxy = proxyLatencies[0].proxy;
-  $done({ action: "select", node: bestProxy.name });
-})();
+proxyLatencies.sort((a, b) => a.latency - b.latency);
+
+const bestProxy = proxyLatencies[0].proxy;
+$done({ action: "select", node: bestProxy.name });
